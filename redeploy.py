1c017e9aac2c46e7e12eacde08d8b228a932936d
Phan <95892957+phansivang@users.noreply.github.com>
2024-10-03 21:17:17 +0700

    Fix: select schema

--- redeploy.py
+++ redeploy.py
@@ -6,9 +6,11 @@ from snowflake.core import Root
 creds = snowflake_creds()
 
 root = Root(snowflake_connector())
-root.session.use_database(creds['server_reset_db']) # select database
 root.session.use_role(creds['role']) # select role
+root.session.use_database(creds['server_reset_db']) # select database
+root.session.use_schema(creds['server_reset_schema']) # select schema
+# select database and schema where is your procedure being stored.
 
-root.session.sql(creds['server_reset_query']).collect() # call procedure function
+root.session.sql('CALL RESTART_SERVER_DEV();').collect() # call procedure function
 print("Server reset successfully")
 
